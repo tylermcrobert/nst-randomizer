d6a29c5eba3ad94dd09633c6a3fd271bb1e7a2fa
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-01-27 14:10:14 -0800

    constants

--- components/App.tsx
+++ components/App.tsx
@@ -3,6 +3,7 @@ import Rider from "./Rider";
 import RiderList from "./RiderList";
 import s from "./App.module.scss";
 import { shuffle } from "../util/shuffle";
+import { RIDERS } from "../constants";
 
 export type Rider = {
   name: string;
@@ -10,33 +11,6 @@ export type Rider = {
 };
 
 export type RiderAndPos = Rider & { rotation: number };
-
-const RIDERS = [
-  { name: "Anna Gasser", fileName: "AnnaGasser.jpg" },
-  { name: "Austen Sweetin", fileName: "AustenSweetin.jpg" },
-  { name: "Ben Ferguson", fileName: "BenFerguson.jpg" },
-  { name: "Blake Paul", fileName: "BlakePaul.jpg" },
-  { name: "Bode Merrill", fileName: "BodeMerrill.jpg" },
-  { name: "Chris Rasman", fileName: "ChrisRasman.jpg" },
-  { name: "Elena Hight", fileName: "ElenaHight.jpg" },
-  { name: "Elias Elhardt", fileName: "EliasElhardt.jpg" },
-  { name: "Eric Jackson", fileName: "EricJackson.jpg" },
-  { name: "Gigi Ruf", fileName: "GigiRuf.jpg" },
-  { name: "Hailey Langland", fileName: "HaileyLangland.jpg" },
-  { name: "Hana Beaman", fileName: "HanaBeaman.jpg" },
-  // { name: "Jake Blauvelt", fileName: "JakeBlauvelt.jpg" },
-  { name: "Jamie Anderson", fileName: "JamieAnderson.jpg" },
-  { name: "Marion Haerty", fileName: "MarionHaerty.jpg" },
-  { name: "Mark McMorris", fileName: "MarkMcMorris.jpg" },
-  { name: "Mikkel Bang", fileName: "MikkelBang.jpg" },
-  { name: "Nils Mindnich", fileName: "NilsMindnich.jpg" },
-  { name: "Pat Moore", fileName: "PatMoore.jpg" },
-  { name: "Robin VanGyn", fileName: "RobinVanGyn.jpg" },
-  { name: "Sage Kotsenburg", fileName: "SageKotsenburg.jpg" },
-  { name: "Travis Rice", fileName: "TravisRice.jpg" },
-  { name: "Victor DeLaRue", fileName: "VictorDeLaRue.jpg" },
-  { name: "Werni Stock", fileName: "WerniStock.jpg" },
-];
 
 const ridersAndPositions: RiderAndPos[] = RIDERS.map((item, i) => ({
   ...item,
